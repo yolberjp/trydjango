949a5d8db4e8e8a07f27a283ac975af0b216d16c
yolberjp <yolberjp@gmail.com>
2020-06-28 23:28:55 +0200

    User login and register

--- user/views.py
+++ user/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'You account has been created! You are now able to login')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
     return render(request, 'user/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'user/profile.html')
